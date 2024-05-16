684ed8e9583761dd3a5ba770f4f22ffb742a5d53
Youssouf Boudouia <youssouf.boudouia@gmail.com>
2024-05-16 15:35:36 +0200

    test: pipeline configuration draft
    this commit is juste to demonstrate the commit '3f05b7d3f8bd3f97ccdf5ebfe0f6f2f4d57c2662' in ragtime-package

--- google_nq/main.py
+++ google_nq/main.py
@@ -3,7 +3,16 @@ PROJECT_NAME:str = "google_nq"
 import ragtime
 from ragtime import expe, generators
 from ragtime.expe import QA, Chunks, Prompt, Question, WithLLMAnswer, UpdateTypes, Answers
-from ragtime.generators import StartFrom, PptrFactsFR, PptrEvalFR, PptrAnsBase, AnsGenerator, EvalGenerator
+from ragtime.generators import (
+    StartFrom,
+    LLMs_from_names, LLM,
+    AnsGenerator, EvalGenerator,
+    run_pipeline,
+)
+from ragtime.prompters import (
+    prompt_table,
+    PptrFactsFR, PptrEvalFR, PptrAnsBase,
+)
 from ragtime.expe import Expe
 
 # always start with init_project before importing ragtime.config values since they are updated
@@ -17,28 +26,91 @@ ragtime.config.init_win_env(['OPENAI_API_KEY', 'ALEPHALPHA_API_KEY', 'ANTHROPIC_
 
 
 logger.debug('MAIN STARTS')
+llms_name:list[str] = [
+    "gpt-4",
+    "vertex_ai/gemini-pro",
+    "mistral/mistral-large-latest",
+    "groq/llama3-8b-8192",
+    "groq/llama3-70b-8192",
+    "groq/mixtral-8x7b-32768",
+    "groq/gemma-7b-it"
+]
 
-ans_gen:AnsGenerator = AnsGenerator(retriever=None,
-                                    llm_names=["gpt-4", 'vertex_ai/gemini-pro', "mistral/mistral-large-latest",
-                                              "groq/llama3-8b-8192", "groq/llama3-70b-8192",
-                                              "groq/mixtral-8x7b-32768", "groq/gemma-7b-it"],
-                                    prompter=PptrAnsBase())
-expe:Expe = generators.generate(text_generator=ans_gen,
-                                folder_in=FOLDER_FACTS,
-                                folder_out=FOLDER_ANSWERS,
-                                json_file="validation_set--30Q_0C_219F_0M_0A_0HE_0AE_2024-05-02_17h30,58.json",
-                                save_to_html=True,
-                                save_to_spreadsheet=True,
-                                template_spreadsheet_path=FOLDER_SST_TEMPLATES / 'without_retriever.xlsx')
+export:dict = {
+    'html': dict(),
+    'spreadsheet': {
+        'path': FOLDER_SST_TEMPLATES / 'without_retriever.xlsx'
+    }
+}
 
-eval_gen:EvalGenerator = EvalGenerator(llm_names=["gpt-4"], prompter=PptrEvalFR())
-expe = generators.generate(text_generator=eval_gen,
-                           folder_in=FOLDER_ANSWERS,
-                           folder_out=FOLDER_EVALS,
-                           json_file=expe.json_path.stem + '.json',
-                        #    json_file='validation_set--30Q_0C_219F_7M_209A_0HE_0AE_2024-05-11_19h51,17.json',
-                           save_to_html=True,
-                           save_to_spreadsheet=True,
-                           template_spreadsheet_path=FOLDER_SST_TEMPLATES / 'without_retriever.xlsx')
+configuration:dict = {
+    'retriever': None,
+    'folder': FOLDER_FACTS,
+    'file_name': "validation_set--30Q_0C_219F_0M_0A_0HE_0AE_2024-05-02_17h30,58.json",
+    'generate': {
+        'answers': {
+            'llms': LLMs_from_names(llms_name, PptrAnsBase()),
+            'folder': FOLDER_ANSWERS,
+            'export': export,
+        },
+        'evals': {
+            'llms': LLMs_from_names(["gpt-4"], PptrEvalFR()),
+            'folder': FOLDER_EVALS,
+            'export': export,
+        }
+    }
+}
+
+run_pipeline(configuration = configuration)
 
 logger.debug('MAIN ENDS')
+
+
+
+#def old_version():
+#    llms_name:list[str] = [
+#        "gpt-4",
+#        "vertex_ai/gemini-pro",
+#        "mistral/mistral-large-latest",
+#        "groq/llama3-8b-8192",
+#        "groq/llama3-70b-8192",
+#        "groq/mixtral-8x7b-32768",
+#        "groq/gemma-7b-it"
+#    ]
+#    llms:list[LLM] = LLMs_from_names(
+#        names = llms_name,
+#        prompter = prompt_table['PptrAnsBase']()
+#    )
+#
+#    ans_gen:AnsGenerator = AnsGenerator(
+#        retriever = None,
+#        llms = llms,
+#    )
+#
+#    expe:Expe = generators.generate(
+#        text_generator = ans_gen,
+#        folder_in = FOLDER_FACTS,
+#        folder_out = FOLDER_ANSWERS,
+#        json_file = "validation_set--30Q_0C_219F_0M_0A_0HE_0AE_2024-05-02_17h30,58.json",
+#        save_to_html = True,
+#        save_to_spreadsheet = True,
+#        template_spreadsheet_path = FOLDER_SST_TEMPLATES / 'without_retriever.xlsx'
+#    )
+#
+#    eval_gen:EvalGenerator = EvalGenerator(
+#        llms = LLMs_from_names(
+#            names = ["gpt-4"],
+#            prompter = prompt_table['PptrEvalFR']()
+#        ),
+#    )
+#
+#    expe = generators.generate(
+#        text_generator = eval_gen,
+#        folder_in = FOLDER_ANSWERS,
+#        folder_out = FOLDER_EVALS,
+#        json_file = expe.json_path.stem + '.json',
+#        #    json_file='validation_set--30Q_0C_219F_7M_209A_0HE_0AE_2024-05-11_19h51,17.json',
+#        save_to_html = True,
+#        save_to_spreadsheet = True,
+#        template_spreadsheet_path = FOLDER_SST_TEMPLATES / 'without_retriever.xlsx'
+#    )
